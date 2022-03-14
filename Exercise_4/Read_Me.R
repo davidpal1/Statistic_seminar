90d6c58930cde5c98fa30e56d237d5048103895c
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-03-14 13:28:28 +0100

    Read before following further

--- Exercise_4/Read_Me.R
+++ Exercise_4/Read_Me.R
@@ -1,8 +1,13 @@
-#Try editing this R code as your first task
+#Try editing  end of this R code as your first task
 #to see if everything is working for you.
-#
 
-#David Paleček - done 
-#Pavel
-#Veronika Funková
-#
+#In folder Exercise_4 are EX4.sh - copied rf_karolina_pbs.sh with few minor changes
+# and EX4 - only copied rf_cv_mc.r - so far unedited
+
+#In addition there are another R scripts that were used during exercises
+
+# Tried 1st edditing
+#David Palecek - done 
+#Pavel XXX 
+#Veronika Funkova 
+#Matej Lebeda
