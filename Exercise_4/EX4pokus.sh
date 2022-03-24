330232af03f26727c1db58cb51ae3dd22c4c68ee
VeronikaFunkova <veronikafunkova@gmail.com>
2022-03-24 12:17:24 +0100

    play

--- Exercise_4/EX4pokus.sh
+++ Exercise_4/EX4pokus.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -N rf
+#PBS -N rfp
 #PBS -l select=1:ncpus=128,walltime=00:05:00
 #PBS -q qexp
-#PBS -e rf.e
-#PBS -o rf.o
+#PBS -e rfp.e
+#PBS -o rfp.o
 
 cd ~/Statistic_seminar/Exercise_4
 pwd
@@ -12,6 +12,7 @@ module load R
 echo "loaded R"
 
 
+time Rscript EX4pokus.r 16
 time Rscript EX4pokus.r 32
 time Rscript EX4pokus.r 64
 time Rscript EX4pokus.r 128
