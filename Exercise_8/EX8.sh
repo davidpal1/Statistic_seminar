a58bad6fcaf0eda15f2f8f3f3705f1ec4bda53ca
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-04-20 19:16:19 +0200

    mcapply funguje

--- Exercise_8/EX8.sh
+++ Exercise_8/EX8.sh
@@ -12,6 +12,9 @@ module load R
 echo "loaded R"
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 
+export OMPI_MCA_mpi_warn_on_fork=0`  
+export RDMAV_FORK_SAFE=1`  
+
 time Rscript mnist_svd_mv.R
 
 
