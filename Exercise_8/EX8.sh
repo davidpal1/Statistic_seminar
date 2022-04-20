d85b2be5e8dc2c157ab8ebc0f5564036892c17cd
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-04-20 21:01:32 +0200

    werqww

--- Exercise_8/EX8.sh
+++ Exercise_8/EX8.sh
@@ -15,6 +15,6 @@ module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0
 export OMPI_MCA_mpi_warn_on_fork=0`  
 export RDMAV_FORK_SAFE=1`  
 
-time mpirun --map-by ppr:32:node Rscript mnist_svd_mv.R --args 4 32
+time mpirun --map-by ppr:32:node Rscript EX8_pracovni.R --args 4 32
 
 
