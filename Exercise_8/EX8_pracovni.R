6ea45d265e7e358dc1549f87ed37727cd9ff7a5b
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-04-21 22:06:57 +0200

    zj

--- Exercise_8/EX8_pracovni.R
+++ Exercise_8/EX8_pracovni.R
@@ -146,7 +146,7 @@ if(comm.rank() == 1) { pdf("Crossvalidation01.pdf")
   ggplot(data.frame(pct = pars, error = cv_err_par/nrow(train)), 
          aes(pct, error)) + geom_point() + geom_smooth() +
     labs(title = "Loess smooth with 95% CI of crossvalidation")
-  pdf(paste0("croSSval", nc, ".pdf"))}
+  devoff()}
 
 
 
