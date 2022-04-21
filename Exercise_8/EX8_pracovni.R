7b925f1b5bbe018bd37d2f06dedbae82b88e4be5
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-04-21 22:55:21 +0200

    afa

--- Exercise_8/EX8_pracovni.R
+++ Exercise_8/EX8_pracovni.R
@@ -162,7 +162,7 @@ predicts = predict_svdmod(test, models2)
 correct <- sum(predicts == test_lab)
 cat("Proportion Correct:", correct/nrow(test), "\n")
 
-if(comm.rank() == 1) {cat(cv, "\n",cv_err_par,"\n"}
+cat(cv, "bylo CV \n",cv_err_par,"bylo nejlepsi veci\n"
 
 finalize()
 
