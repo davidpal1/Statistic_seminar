082bbbf7fa41fd4ee842bf08b3f72ec98480017b
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-04-21 09:01:54 +0200

    Co naklikat v karoline aby to fungovalo

---
+++ Jak_zprovoznit_karolinu_sooubory.R
@@ -0,0 +1,11 @@
+# Co udelat aby veci fungovaly:
+
+#dostat se do karoliny a :
+
+#module load R
+# R
+
+remotes::install_github("RBigData/pbdIO")
+
+install.packages("BioManager")
+BiocManager::install("rhdf5")
