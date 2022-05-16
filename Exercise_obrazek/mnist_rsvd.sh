4451dbad6b9d2ba78c13dc20f10a6d104b86b2e5
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-05-16 11:37:22 +0200

    afasdf

--- Exercise_obrazek/mnist_rsvd.sh
+++ Exercise_obrazek/mnist_rsvd.sh
@@ -20,6 +20,9 @@ module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore
 
 
 ## --args blas fork
+time mpirun --map-by ppr:1:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:2:node Rscript mnist_rsvd.R
 time mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
 time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
 time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
+time mpirun --map-by ppr:32:node Rscript mnist_rsvd.R
