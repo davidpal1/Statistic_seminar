0a52f4c980c494c910b09f86ca6a0fe77597ab80
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-05-16 14:17:49 +0200

    dfa

--- Exercise_obrazek/delani_obrazku.R
+++ Exercise_obrazek/delani_obrazku.R
@@ -1,5 +1,6 @@
 
 jadra <- c(1,2,4,8,16,32)
 casy <- c(3.343,8.574,15.792,18.958,35.765, 67.835)
-plot(y=casy,x=jadra,pch=15,col="red",cex=2,main="Čas programu při daném počtu jader")
-lines(jadra,casy,col="red")
+casyr <- c(7.322,8.188,4.305,4.198, 4.470,6.387)
+plot(y=casyr,x=jadra,pch=15,col="red",cex=2,main="Čas programu při daném počtu jader")
+lines(jadra,casyr,col="red")
