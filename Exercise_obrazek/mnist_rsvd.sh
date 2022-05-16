b2ba909b7375474f85521f25572cb1c784bfe542
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-05-16 11:27:13 +0200

    fadsf

--- Exercise_obrazek/mnist_rsvd.sh
+++ Exercise_obrazek/mnist_rsvd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N mnist_rsvd
-#PBS -l select=1:ncpus=128,walltime=00:10:00
+#PBS -l select=1:ncpus=128,walltime=00:15:00
 #PBS -q qexp
 #PBS -e mnist_rsvd.e
 #PBS -o mnist_rsvd.o
@@ -17,4 +17,6 @@ time Rscript mnist_rsvd.R --args 4 16
 time Rscript mnist_rsvd.R --args 4 32
 time Rscript mnist_rsvd.R --args 4 64
 time Rscript mnist_rsvd.R --args 4 128
+
+time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
 
