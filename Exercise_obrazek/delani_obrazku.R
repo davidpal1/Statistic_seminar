3a485ff787a7ec56c635699c523ccbad38412944
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-05-16 12:00:43 +0200

    afs

--- Exercise_obrazek/delani_obrazku.R
+++ Exercise_obrazek/delani_obrazku.R
@@ -1,5 +1,5 @@
 
 jadra <- c(1,2,4,8,16,32)
 casy <- c(3.343,8.574,15.792,18.958,35.765, 67.835)
-obr <- plot(y=casy,x=jadra)
-dev.off()
+plot(y=casy,x=jadra,pch=15,col="red",cex=2,main="Čas programu při daném počtu jader")
+lines(jadra,casy,col="red")
