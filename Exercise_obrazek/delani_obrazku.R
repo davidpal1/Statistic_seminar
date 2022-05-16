6d3d01b20c9f65647595b73e28190e092f78ae94
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-05-16 14:23:16 +0200

    rag

--- Exercise_obrazek/delani_obrazku.R
+++ Exercise_obrazek/delani_obrazku.R
@@ -2,5 +2,6 @@
 jadra <- c(1,2,4,8,16,32)
 casy <- c(3.343,8.574,15.792,18.958,35.765, 67.835)
 casyr <- c(7.322,8.188,4.305,4.198, 4.470,6.387)
-plot(y=casyr,x=jadra,pch=15,col="red",cex=2,main="Čas programu při daném počtu jader")
+plot(y=casyr,x=jadra,pch=15,col="red",cex=2,main="Run time",
+     ylim=c(3,9),ylab="time[s]",xlab="N. of cores")
 lines(jadra,casyr,col="red")
