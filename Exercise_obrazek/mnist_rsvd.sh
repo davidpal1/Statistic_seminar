51475996559ffdd2c13c6fd37d8fe3d51b529c42
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-05-16 11:33:28 +0200

    sad

--- Exercise_obrazek/mnist_rsvd.sh
+++ Exercise_obrazek/mnist_rsvd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -N mnist_rsvd
-#PBS -l select=1:ncpus=128,walltime=00:15:00
+#PBS -l select=1:mpiprocs=32
+#PBS -l walltime=00:10:00
 #PBS -q qexp
 #PBS -e mnist_rsvd.e
 #PBS -o mnist_rsvd.o
@@ -11,12 +12,14 @@ pwd
 module load R
 echo "loaded R"
 
+## prevent warning when fork is used with MPI
+export OMPI_MCA_mpi_warn_on_fork=0
+export RDMAV_FORK_SAFE=1
+## Fix for warnings from libfabric/1.12 bug
+module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore
+
+
 ## --args blas fork
-time Rscript mnist_rsvd.R --args 4 8
-time Rscript mnist_rsvd.R --args 4 16
-time Rscript mnist_rsvd.R --args 4 32
-time Rscript mnist_rsvd.R --args 4 64
-time Rscript mnist_rsvd.R --args 4 128
-
+time mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
 time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
-
+time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
